85410e80cfa23323b7376e682193619906e2fa61
antongoransson <anton.goransson95@gmail.com>
2022-12-13 19:59:04 +0100

    Update compare function day13

--- day13/day13.py
+++ day13/day13.py
@@ -3,39 +3,31 @@ import sys
 
 
 def compare(l, r):
-    for l_v, r_v in zip(l, r):
-        if type(l_v) is list and type(r_v) is list:
-            c = compare(l_v, r_v)
-            if c != 0:
-                return c
-        elif type(l_v) is int and type(r_v) is int:
-            if int(l_v) < int(r_v):
-                return 1
-            elif int(l_v) > int(r_v):
-                return -1
-        elif type(l_v) is list and type(r_v) is int:
-            c = compare(l_v, [r_v])
-            if c != 0:
-                return c
-        elif type(l_v) is int and type(r_v) is list:
-            c = compare([l_v], r_v)
-            if c != 0:
-                return c
-    if len(l) < len(r):
-        return 1
-    elif len(l) > len(r):
-        return -1
-    return 0
+    match (l[0], r[0]):
+        case a, b if a == b:
+            return compare(l[1:], r[1:])
+        case ']', _:
+            return -1
+        case _, ']':
+            return 1
+        case '[', _:
+            return compare(l, '[' + r[0] + ']' + r[1:])
+        case _, '[':
+            return compare('[' + l[0] + ']' + l[1:], r)
+        case a, b:
+            return ord(a) - ord(b)
 
 
 def solve_part_1(in_data):
-    return sum(i + 1 for i, (l, r) in enumerate(in_data) if compare(l, r) == 1)
+    return sum(i + 1 for i, (l, r) in enumerate(in_data) if compare(l, r) < 0)
 
 
 def solve_part_2(in_data):
-    p = [l for l, r in in_data] + [r for l, r in in_data] + [[2], [6]]
-    p.sort(key=cmp_to_key(compare), reverse=True)
-    return (p.index([2]) + 1) * (p.index([6]) + 1)
+    p = ['[2]', '[6]']
+    for pair in in_data:
+        p.extend(pair)
+    p.sort(key=cmp_to_key(compare))
+    return (p.index('[2]') + 1) * (p.index('[6]') + 1)
 
 
 def main():
@@ -43,7 +35,7 @@ def main():
     in_data = []
     with open(in_f) as f:
         for pair in f.read().split('\n\n'):
-            l, r = map(eval, pair.split('\n'))
+            l, r = pair.replace('10', 'A').split('\n')
             in_data.append((l, r))
     sol1 = solve_part_1(in_data)
     print(f'Part 1: {sol1}')
